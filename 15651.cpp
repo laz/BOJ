6392e8a91c2fdf742265f140a0f9d6ea4da07ff0
laz <danier123@gmail.com>
2020-12-26 22:20:16 +0900

    N과 M(3)

---
+++ 15651.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+
+void printPermutations(vector<int>& p, int length){
+    if(length == M){
+        for(int i = 0; i < p.size(); ++i)
+            printf("%d ", p[i]+1);
+        printf("\n");
+        return ;
+    }
+    for(int i = 0; i < N; ++i){
+        p.push_back(i);
+        printPermutations(p, length+1);
+        p.pop_back();
+    }
+}
+
+int main(){
+    scanf("%d %d", &N, &M);
+    vector<int> p;
+    printPermutations(p, 0);
+}
