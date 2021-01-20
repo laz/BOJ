e1b67e7691e20e9f966c26aeffa5de1a1dfdd2ab
laz <danier123@gmail.com>
2021-01-20 23:59:57 +0900

    큐

---
+++ 18258.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int que[2000001];
+
+int main(){
+    ios_base::sync_with_stdio(false); 
+    cin.tie(NULL);
+    int c, n, head = 0, tail = 0;
+    cin >> c;
+    string op;
+    while(c--){
+        cin >> op;
+        if(op == "push"){
+            cin >> n;
+            que[tail++] = n;
+        }
+        else if(op == "pop"){
+            if(head == tail){
+                cout << -1 << "\n";
+                continue;
+            }
+            cout << que[head] << "\n";
+            que[head++] = 0;
+        }
+        else if(op == "front"){
+            if(head == tail){
+                cout << -1 << "\n";
+                continue;
+            }
+            cout << que[head] << "\n";
+        }
+        else if(op == "back"){
+            if(head == tail){
+                cout << -1 << "\n";
+                continue;
+            }
+            cout << que[tail-1] << "\n";
+        }
+        else if(op == "size")
+            cout << tail-head << "\n";
+        else if(op == "empty"){
+            if(head == tail) cout << "1\n";
+            else cout << "0\n";
+        }
+    }
+}
