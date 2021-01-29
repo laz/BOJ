5d6b25f1e29c2a169e3beece0c2b4c6701ecadc3
laz <danier123@gmail.com>
2021-01-29 23:52:50 +0900

    절댓값 힙

---
+++ 11286.cpp
@@ -0,0 +1,52 @@
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+vector<int> heap;
+
+void push_heap(int n){
+    heap.push_back(n);
+    int idx = heap.size()-1;
+    while(idx > 0 && (abs(heap[(idx-1)/2]) > abs(heap[idx]) || abs(heap[(idx-1)/2]) == abs(heap[idx]) && heap[(idx-1)/2] > heap[idx])){
+        swap(heap[(idx-1)/2], heap[idx]);
+        idx = (idx-1)/2;
+    }
+}
+
+int pop_heap(){
+    if(heap.size() == 0) return 0;
+    int ret = heap[0];
+
+    heap[0] = heap.back();
+    heap.pop_back();
+
+    int here = 0;
+    while(1){
+        int left = here*2+1, right = here*2+2;
+        if(left >= heap.size()) break;
+
+        int next = here;
+        if(abs(heap[left]) < abs(heap[next]) || abs(heap[left]) == abs(heap[next]) && heap[left] < heap[next]) next = left;
+        if(right < heap.size() && (abs(heap[right]) < abs(heap[next]) || abs(heap[right]) == abs(heap[next]) && heap[right] < heap[next])) next = right;
+
+        if(next == here) break;
+        swap(heap[here], heap[next]);
+        here = next;
+    }
+    return ret;
+}
+
+int main(){
+    int n, op;
+    scanf("%d", &n);
+    heap.reserve(n);
+    while(n--){
+        scanf("%d", &op);
+        if(op)
+            push_heap(op);
+        else
+            printf("%d\n", pop_heap());
+    }
+}
