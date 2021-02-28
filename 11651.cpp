7aa47540740171e1cee8a6e5ea7cf75931febe80
laz <danier123@gmail.com>
2021-02-28 20:12:58 +0900

    좌표 정렬하기 2

---
+++ 11651.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0); cin.tie(0);
+    int n, x, y;
+    cin >> n;
+    vector<pair<int, int> > p;
+    for(int i = 0; i < n; ++i){
+        cin >> x >> y;
+        p.push_back({y, x});
+    }
+    sort(p.begin(), p.end());
+    for(auto a : p)
+        cout << a.second << " " << a.first << "\n";
+}
