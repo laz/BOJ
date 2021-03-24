00204ace0ff1638011c7c15a0447b157ebbede19
laz <danier123@gmail.com>
2021-03-24 10:57:15 +0900

    2차원 배열의 합

--- 2167.cpp
+++ 2167.cpp
@@ -1,27 +1,28 @@
 #include <iostream>
 using namespace std;
 
-int map[300][300];
+int map[301][301], psum[301][301];
 
 int main(){
     ios::sync_with_stdio(0); cin.tie(0);
     int n, m;
     cin >> n >> m;
-    for(int i = 0; i < n; ++i){
-        for(int j = 0 ; j < m; ++j){
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1 ; j <= m; ++j){
             cin >> map[i][j];
         }
     }
+    
+    for(int i = 1; i <= n; ++i){
+        for(int j = 1; j <= m; ++j){
+            psum[i][j] = map[i][j] + psum[i-1][j] + psum[i][j-1] - psum[i-1][j-1];
+        }
+    }
+
     int t, x1, x2, y1, y2;
     cin >> t;
     while(t--){
         cin >> x1 >> y1 >> x2 >> y2;
-        int res = 0;
-        for(int i = x1-1; i <= x2-1; ++i){
-            for(int j = y1-1; j <= y2-1; ++j){
-                res += map[i][j];
-            }
-        }
-        cout << res << "\n";
+        cout << psum[x2][y2] - psum[x2][y1-1] - psum[x1-1][y2] + psum[x1-1][y1-1] << "\n";
     }
 }
