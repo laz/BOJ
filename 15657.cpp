fa01ca5eeeba6b2fef1f2fe5281ef45812768983
laz <21600293@handong.edu>
2021-04-16 17:27:19 +0900

    N과 M (8)

---
+++ 15657.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+using namespace std;
+
+int arr[8], save[8];
+int n, m;
+
+void input() {
+    fastio;
+    cin >> n >> m;
+    for(int i = 0; i < n; ++i) cin >> arr[i];
+}
+
+void d(int idx, int cnt){
+    if(cnt == m) {
+        for(int i = 0; i < m; ++i) cout << save[i] << " "; cout << "\n";
+        return ;
+    }
+
+    for(int i = idx; i < n; ++i){
+        save[cnt] = arr[i];
+        d(i, cnt+1);
+    }
+}
+
+int main(){
+    input();
+    sort(arr, arr+n);
+    d(0, 0);
+}
