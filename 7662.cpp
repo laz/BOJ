55f80c5c0f40f7f61fb41f9f88ec6e810c759941
laz <danier123@gmail.com>
2021-05-06 00:33:36 +0900

    이중 우선순위 큐

--- 7662.cpp
+++ 7662.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 #define fastio ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
-using ll = long long;
+
 int t, n;
 
 void input() {
@@ -11,42 +11,49 @@ void input() {
 
 int main() {
     input();
-    ll num;
+    int num;
     char s;
 
     while(t--) {
         cin >> n;
 
-        priority_queue<pair<ll, int> > minHeap, maxHeap;
+        priority_queue<pair<int, int>, vector<pair<int, int> >, greater<pair<int, int> > > minHeap;
+        priority_queue<pair<int, int> > maxHeap;
         vector<bool> valid(n, true);
-
+        int cnt = 0;
         for(int i = 0; i < n; ++i) {
             cin >> s >> num;
 
             if(s == 'I') {
-                minHeap.push({-num, i});
+                minHeap.push({num, i});
                 maxHeap.push({num, i});
+                ++cnt;
             }
             else {
+                if(cnt == 0) continue;
                 if(num == 1) {
-                    while(!maxHeap.empty() && !valid[maxHeap.top().second]) maxHeap.pop();
-                    if(maxHeap.empty()) continue;
+                    while(!maxHeap.empty() && !valid[maxHeap.top().second]) 
+                        maxHeap.pop();
+                    
                     valid[maxHeap.top().second] = false;
                     maxHeap.pop();
                 }
                 else {
-                    while(!minHeap.empty() && !valid[minHeap.top().second]) minHeap.pop();
-                    if(minHeap.empty()) continue;
+                    while(!minHeap.empty() && !valid[minHeap.top().second]) 
+                        minHeap.pop();
+                        
                     valid[minHeap.top().second] = false;
                     minHeap.pop();
                 }
+                --cnt;
             }
         }
 
-        while(!minHeap.empty() && !valid[minHeap.top().second]) minHeap.pop();
-        while(!maxHeap.empty() && !valid[maxHeap.top().second]) maxHeap.pop();
-
-        if(minHeap.empty()) cout << "EMPTY\n";
-        else cout << maxHeap.top().first << " " << -minHeap.top().first << "\n";
+        if(cnt == 0) cout << "EMPTY\n";
+        else {
+            while(!minHeap.empty() && !valid[minHeap.top().second]) minHeap.pop();
+            while(!maxHeap.empty() && !valid[maxHeap.top().second]) maxHeap.pop();
+            cout << maxHeap.top().first << " " << minHeap.top().first << "\n";
+        }
     }
 }
