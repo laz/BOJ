1d92dbad3ae1a9e5995689dc22756aa05607b189
laz <21600293@handong.edu>
2021-05-07 14:21:45 +0900

    오큰수

--- 17298.cpp
+++ 17298.cpp
@@ -1,36 +1,35 @@
-#include <cstdio>
-#include <stack>
-#include <vector>
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
 using namespace std;
 
-int main(){
-    int n, read, tmp;
-    scanf("%d", &n);
-    stack<int> nge;
-    stack<int> print;
-    vector<int> ans;
-    for(int i = 0; i < n; ++i){
-        scanf("%d", &read);
-        while(!nge.empty() && nge.top() < read){
-            if(ans.back() != -1){
-                print.push(ans.back());
-                ans.pop_back();
-                continue;
-            }
-            else
-                print.push(read);
-            ans.pop_back();
-            nge.pop();
+int arr[1000001];
+int n;
+
+void input() {
+    fastio;
+    cin >> n;
+}
+
+int main() {
+    input();
+
+    stack<pair<int, int> > stk;
+    for(int i = 0; i < n; ++i) {
+        int num; cin >> num;
+        
+        while(!stk.empty() && stk.top().first < num) {
+            arr[stk.top().second] = num;
+            stk.pop();
         }
-        while(!print.empty()){
-            tmp = print.top();
-            ans.push_back(tmp);
-            print.pop();
-        }
-        nge.push(read);
-        ans.push_back(-1);
+        stk.push({num, i});
     }
-    for(auto e : ans)
-        printf("%d ", e);
-    printf("\n");
+
+    while(!stk.empty()) {
+        arr[stk.top().second] = -1;
+        stk.pop();
+    }
+
+    for(int i = 0; i < n; ++i) {
+        cout << arr[i] << " ";
+    }
 }
