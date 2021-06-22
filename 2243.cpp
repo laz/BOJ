aa47f9a8803b16d5ac1f89e8b1a4465f121dcd90
laz <21600293@handong.edu>
2021-06-22 21:32:26 +0900

    사탕상자

---
+++ 2243.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define fastio ios::sync_with_stdio(0), cin.tie(0)
+using namespace std;
+
+int tree[1<<21];
+int n;
+
+void input() {
+    fastio;
+    cin >> n;
+}
+
+int update(int t, int v, int i = 1, int s = 0, int e = (1<<20)-1) {
+    if(t < s || t > e) return tree[i];
+    if(s == e) return tree[i] += v;
+    int m = (s + e) >> 1;
+    return tree[i] = update(t, v, i<<1, s, m) + update(t, v, i<<1|1, m+1, e);
+}
+
+int query(int k, int i = 1, int s = 0, int e = (1<<20)-1) {
+    if(s == e) return s;
+    int m = (s + e) >> 1;
+    if(k <= tree[i<<1]) return query(k, i<<1, s, m);
+    return query(k-tree[i<<1], i<<1|1, m+1, e);
+}
+
+int main() {
+    input();
+    while(n--) {
+        int a, b; cin >> a >> b;
+        if(a == 1) {
+            int idx = query(b);
+            cout << idx+1 << "\n";
+            update(idx, -1);
+        }
+        else {
+            int c; cin >> c;
+            update(b-1, c);
+        }
+    }   
+}
